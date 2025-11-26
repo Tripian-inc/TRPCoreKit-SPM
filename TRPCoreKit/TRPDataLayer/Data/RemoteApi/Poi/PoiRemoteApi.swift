695a4cf6609a57fb46ed549674330ae3a8efd031
cmtripian <cem@tripian.com>
2025-11-26 20:46:52 +0300

    Clean up repository: update .gitignore and remove unnecessary files
    - Updated .gitignore with comprehensive iOS/Swift patterns
    - Removed build artifacts (.build/, .swiftpm/)
    - Removed temporary files (tmp.xcconfig)
    - Removed generated dependency files (Cartfile.resolved, Package.resolved)
    - Removed redundant archives (Assets.xcassets.zip)
    - Removed database files (TrpUsers.sqlite)

--- TRPCoreKit/TRPDataLayer/Data/RemoteApi/Poi/PoiRemoteApi.swift
+++ TRPCoreKit/TRPDataLayer/Data/RemoteApi/Poi/PoiRemoteApi.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import TRPFoundationKit
 
 
 public protocol PoiRemoteApi {
--- TRPCoreKit/TRPDataLayer/Data/RemoteApi/Questions/TRPQuestionRemoteApi.swift
+++ TRPCoreKit/TRPDataLayer/Data/RemoteApi/Questions/TRPQuestionRemoteApi.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import TRPRestKit
 
 public class TRPQuestionRemoteApi: QuestionRemoteApi {
     
